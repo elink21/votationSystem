ca184104719d4138bea6a17b744dd0c561730597
elink211 <eliassegundo.segundo@gmail.com>
2021-04-04 23:32:02 -0500

    Login/logout synced, realtime graph and votation pending

--- votation/urls.py
+++ votation/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.login, name="index"),
-    path('realTime', views.realTime, name="realTime")
+    path('', views.login, name="login"),
+    path('vote', views.vote, name="vote"),
+    path('realTime', views.realTime, name="realTime"),
+    path('logout', views.logout, name="logout")
 ]
--- votation/views.py
+++ votation/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from votation.models import UserEncrypted, django_system
 # Create your views here.
 
 
 def login(request):
-    return render(request, "login.html", {})
+    return render(request, 'login.html', {})
+
+
+def logout(request):
+    if 'userName' in request.session.keys():
+        del request.session['userName']
+    return render(request, 'login.html', {})
+
+
+def vote(request):
+    userName = request.POST['userName']
+    password = request.POST['password']
+    matches = django_system.objects.filter(name=userName)
+    if(matches.count() == 1 and django_system.objects.get(name=userName).password == password):
+        resultUser = django_system.objects.get(name=userName)
+        request.session['userName'] = resultUser.name
+        return render(request, "vote.html", {'userName': resultUser.name, 'vote': resultUser.vote})
+    else:
+        return redirect('login')
 
 
 def realTime(request):
